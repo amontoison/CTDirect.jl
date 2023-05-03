4ad6c1ddad50ef19969330d097108135ba71a419
PierreMartinon <pierrecmartinon@gmail.com>
2023-05-03 18:18:44 +0200

    gauss 2 does not converge; todo: recheck trapeze convergence after disabling problematic interpolations

--- test/manual_test_exponential_tmin.jl
+++ test/manual_test_exponential_tmin.jl
@@ -6,5 +6,5 @@ using CTProblems
 # simple integrator - time min
 prob = Problem(:exponential, :time, :state_dim_1, :control_dim_1, :lagrange)
 ocp = prob.model
-sol = solve(ocp, grid_size=100, print_level=5, tol=1e-12, mu_strategy="adaptive", init=nothing)
+sol = solve(ocp, grid_size=100, print_level=5, tol=1e-12, rk_method=:gauss2)
 p1 = plot(sol)
